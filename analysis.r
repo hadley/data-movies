c219049ee55c630a1abab13ca26c793df2f15f7f
hadley <h.wickham@gmail.com>
2005-05-23 22:23:16 +0000

    More distributional stats, updated db backup
    git-svn-id: svn://had.co.nz/projects/movies@782 688fc0e7-72e3-0310-a89e-c7106b9812cc

--- analysis.r
+++ analysis.r
@@ -3,6 +3,10 @@ setwd("~/documents/movies")
 
 m <- read.csv("movies.csv")
 
-xyplot(length ~ year, m)
-xyplot(budget ~ year, m)
-xyplot(log(budget) ~ year, m)
+head(m)
+cnt.col <- which(sapply(m, is.numeric))
+cat.col <- which(sapply(m, is.factor))
+
+
+cnt.sum <- t(sapply(m[, cnt.col], function(x) c(range(x, na.rm=T), sum(!is.na(unique(x))), sum(is.na(x)))))
+colnames(cnt.sum) <- c("Minimum","Maximum","Unique values","Missing values")
