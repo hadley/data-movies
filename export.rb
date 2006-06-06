6d2296fc7b862960b942968fea10cbcf61c452b2
hadley <h.wickham@gmail.com>
2006-06-06 10:24:25 +0000

    Final update to movies data

--- export.rb
+++ export.rb
@@ -1,60 +1,47 @@
 require "rubygems"
 require "arrayfields"
-require "FasterCSV"
 require "sqlite3"
+require "set"
 
 $genres_of_interest = ["Action", "Animation", "Comedy", "Drama", "Documentary", "Romance", "Short"]
-$ratings_map = {"." => 0, "0" => 4.5, "1" => 14.5, "2" => 24.5, "3" => 34.5, "4" => 44.5, "5" => 45.5, "6" => 64.5, "7" => 74.5, "8" => 84.5, "9" => 94.5, "*" => 10}
+$ratings_map = {"." => 0, "0" => 4.5, "1" => 14.5, "2" => 24.5, "3" => 34.5, "4" => 44.5, "5" => 45.5, "6" => 64.5, "7" => 74.5, "8" => 84.5, "9" => 94.5, "*" => 100}
 
 
-def genres_binary(genres)
-	genres = genres.to_set
-	$genres_of_interest.map { |genre| genres.include? genre}
-	end
+def genres_binary(id, db)
+	genres = db.execute("SELECT genre FROM Genres where movie_id = #{id};").flatten.to_set
+	$genres_of_interest.map { |genre| (genres.include? genre) ? 1 : 0}
 end
 
 def ratings_breakdown(ratings)
-	ratings[2..ratings.length].to_s.split(//).map{|s| $ratings.map[s]}
+	ratings[0..ratings.length].to_s.split(//).map{|s| $ratings_map[s]}
 end
-
 
 db = SQLite3::Database.new( "movies.sqlite3" )
+sql = "
+	SELECT Movies.* 
+	FROM Movies
+	WHERE length > 0 and imdb_votes > 0
+	ORDER BY title"
 
-default = [0] * $genres_of_interest.length
-db.create_aggregate( "genres", 1 ) do
-	step do |func, value|
-		func[ :total ] ||= default.clone
-		func[ :total ][$genres_of_interest.index(value.to_s)] = 1 if $genres_of_interest.index(value.to_s) rescue nil
+i = 0 
+
+File.open("movies.tab", "w") do |out|
+	out << [
+		'title', 'year', 'length', 'budget', 
+		'rating', 'votes', (1..10).map{|i| "r" + i.to_s}, 
+		'mpaa', $genres_of_interest
+	].flatten.join("\t") + "\n"
+
+	db.execute(sql) do |row| 
+		puts i if (i = i + 1) % 5000 == 0
+
+		out << [
+			row["title"], 
+			row["year"], 
+			row["length"], 
+			row["budget"], 
+			row["imdb_rating"], row["imdb_votes"], ratings_breakdown(row["imdb_rating_votes"]), 
+			row["mpaa_rating"], genres_binary(row['id'], db)
+		].flatten.join("\t") + "\n" rescue nil
 	end
-
-	finalize do |func|
-		if (func[ :total ])
-			func.set_result( func[ :total ].join(",") )
-		else
-			func.set_result( default.join(",") )
-		end
-		func[:total] = nil
-	end
-end
-
-sql = "SELECT *, (select genres(genre) from Genres where movie_id = Movies.id) as g FROM Movies WHERE length >0 and imdb_votes > 0"
-
-
-db.execute(sql + " limit 500;") do |row|
-	puts row["id"]
-end
-
-FasterCSV.open("movies.csv", "w") do |csv|
- 	csv << [
- 		'title', 'year', 'budget', 'length', 
- 		'rating', 'votes', (1..10).map{|i| "r" + i.to_s}, 
- 		'mpaa', $genres_of_interest].flatten
-
- 	db.execute(sql) do |row| 
- 		csv << [
- 			row["title"], row["year"], row["budget"], row["length"], 
- 			row["imdb_rating"], row["imdb_votes"], ratings_breakdown(row["imdb_rating_votes"]), 
- 			row["mpaa_rating"], row["g"].split()
- 		].flatten rescue nil
- 	end
 end
--- import.rb
+++ import.rb
@@ -1,15 +1,19 @@
-require "movies"
+require 'rubygems'
+require 'sqlite3'
+$db = SQLite3::Database.new( "movies.sqlite3" )
+$title = "[a-z,&-;0-9$#+=\/!?. ]+"
 
 def import_movies
 	#$100,000 Pyramid, The (2001) (VG)			2001
-	title_re = /^([a-zA-z ]+)\s+\([0-9]+\)\s+([0-9]+)$/ix
-	File.new("movies.list").each_line do |l|
-		if  match = title_re.match(l)
-			m = Movie.new("title" => match[1], "year" => match[2].to_i)
-			m.save
-		
-			if m.id % 200 == 0
-				puts m.id
+	title_re = /^(#{$title}) \s+ \([0-9]+\) \s+ ([0-9]+)$/ix
+
+	stmt = $db.prepare("INSERT INTO Movies (title, year) VALUES (?, ?);")
+	$db.transaction do
+		$db.execute "DELETE FROM Movies;"
+	
+		File.new("data/movies.list").each_line do |l|
+			if match = title_re.match(l)
+				stmt.execute!(match[1], match[2].to_i)
 			end
 		end
 	end
@@ -17,17 +21,14 @@ end
 
 def import_times
 	#"Ballyskillen Opera House, The" (1980)			30	(6 episodes)
-	time_re = /^([a-z ]+) \s+ \(([0-9]+)\) \s+ ([0-9]+)/ix 
-	File.new("running-times.list").each_line do |l|
-		if match = time_re.match(l) 
-			title, year, minutes =  match[1], match[2].to_i, match[3]
-		
-			m = Movie.find_by_title_and_year title, year
-			if m
-				#puts "#{title} (#{year}) = #{minutes}"
-				m.update_attribute("length", minutes)
+	time_re = /^(#{$title}) \s+ \(([0-9]+)\) \s+ (?:[a-z]+:)?([0-9]+)/ix 
+
+	stmt = $db.prepare("UPDATE Movies set length=? WHERE title=? AND year=?;")
+	$db.transaction do 
+		File.new("data/running-times.list").each_line do |l|
+			if match = time_re.match(l)
+				stmt.execute!(match[3].to_i, match[1], match[2].to_i)
 			end
-		
 		end
 	end
 end
@@ -35,20 +36,14 @@ end
 
 def import_budgets
 	dashes = "-------------------------------------------------------------------------------"
-	title_re = /MV:\s+([a-z ]*?) \s \(([0-9]+)\)/ix
+	title_re = /MV:\s+(#{$title}?) \s \(([0-9]+)\)/ix
 	budget_re = /BT:\s+USD\s+([0-9,.]+)/ix
 
-	File.new("business.list").each(dashes) do |l|
-		if match = title_re.match(l)
-			if bt = budget_re.match(l)
-				title, year, budget = match[1], match[2], bt[1].gsub!(",","").to_i
-
-				m = Movie.find_by_title_and_year title, year
-				if m
-					#puts "#{title} $#{budget}"
-					m.update_attribute("budget", budget)
-				end
-
+	stmt = $db.prepare("UPDATE Movies set budget=? WHERE title=? AND year=?;")
+	$db.transaction do 
+		File.new("data/business.list").each(dashes) do |l|
+			if match = title_re.match(l.to_s) and bt = budget_re.match(l.to_s)
+				stmt.execute!(bt[1].gsub!(",","").to_i, match[1], match[2].to_i) 
 			end
 		end
 	end
@@ -56,20 +51,14 @@ end
 
 def import_mpaa_ratings
 	dashes = "-------------------------------------------------------------------------------"
-	title_re = /MV:\s+([a-z ]*?) \s \(([0-9]+)\)/ix
+	title_re = /MV:\s+(#{$title}?) \s \(([0-9]+)\)/ix
 	rating_re = /RE: Rated (.*?) /i
 
-	File.new("mpaa-ratings-reasons.list").each(dashes) do |l|
-		if match = title_re.match(l)
-			if rt = rating_re.match(l)
-				title, year, rating = match[1], match[2], rt[1]
-
-				m = Movie.find_by_title_and_year title, year
-				if m
-					puts "#{title} #{rating}"
-					m.update_attribute("mpaa_rating", rating)
-				end
-
+	stmt = $db.prepare("UPDATE Movies set mpaa_rating=? WHERE title=? AND year=?;")
+	$db.transaction do 
+		File.new("data/mpaa-ratings-reasons.list").each(dashes) do |l|
+			if match = title_re.match(l.to_s) and rt = rating_re.match(l.to_s)
+				stmt.execute!(rt[1], match[1], match[2].to_i)
 			end
 		end
 	end
@@ -77,35 +66,34 @@ end
 
 
 def import_genres
-#D2: The Mighty Ducks (1994)				Family
-	genre_re = /^([a-z ]*?)\s+\(([0-9]+)\)\s+(.*?)$/ix
-
-	File.new("genres.list").each_line do |l|
-		if match = genre_re.match(l)
-			title, year, genre = match[1], match[2], match[3]
-
-			m = Movie.find_by_title_and_year title, year
-			if m
-				#puts "#{title} $#{genre}"
-				m.genres.create({"genre" => genre})
+	#D2: The Mighty Ducks (1994)				Family
+	genre_re = /^(#{$title}?) \s+ \(([0-9]+)\) (?:\s*[({].*[})])*  \s+(.*?)$/ix
+	i = 0
+	
+	stmt = $db.prepare("INSERT INTO Genres (genre, movie_id) VALUES (?, (SELECT id FROM Movies WHERE title=? AND year=?));")
+	$db.transaction do 
+		$db.execute "DELETE FROM Genres;"
+		
+		File.new("data/genres.list").each_line do |l|
+			puts i if (i = i + 1) % 5000 == 0
+			if match = genre_re.match(l)
+				stmt.execute!(match[3], match[1], match[2].to_i)
 			end
 		end
 	end
 end
 
-require 'sqlite3'
-$db = SQLite3::Database.new( "movies.sqlite3" )
 
 def import_ratings
 	#.0.1112000      14   5.9  365 Nights in Hollywood (1934)
-	ratings_re = /([0-9.\*]+) \s+ ([0-9]+) \s+ ([0-9.]+) \s+ ([a-z ]+?) \s+ \(([0-9]+)\)/ix
-	f = File.new("ratings.list")
+	ratings_re = /([0-9.\*]+) \s+ ([0-9]+) \s+ ([0-9.]+) \s+ (#{$title}?) \s+ \(([0-9]+)\)/ix
+
+	stmt = $db.prepare("UPDATE Movies set imdb_votes=?, imdb_rating=?, imdb_rating_votes=? WHERE title=? AND year=?;")
 	$db.transaction
-	stmt = $db.prepare("UPDATE Movies set imdb_votes=?, imdb_rating=?, imdb_rating_votes='r?' WHERE title='?' AND year=?;")
 	
-	f.each_line do |l|
+	File.new("data/ratings.list").each_line do |l|
 		if match = ratings_re.match(l)
-			rating, votes, outof10, title, year = match[1], match[2], match[3].to_f, match[4], match[5]
+			rating, votes, outof10, title, year = match[1], match[2], match[3], match[4], match[5]
 			stmt.execute!(votes, outof10, rating, title, year)
 		end
 	end
@@ -114,10 +102,10 @@ def import_ratings
 end
 
 #import_movies
-import_ratings
 #import_times
-#import_mpaa_ratings
 #import_budgets
+#import_mpaa_ratings
+#import_ratings
 #import_genres
 
 
